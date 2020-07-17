7d58603ebb4bce43eff1417b6886308df0cb2b51
kkito <kkitokkito@126.com>
2020-07-17 09:39:31 +0800

    socks support

---
+++ demo/socks.go
@@ -0,0 +1,50 @@
+package main
+
+// demo from https://play.golang.org/p/l0iLtkD1DV
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/proxy"
+)
+
+const (
+	PROXY_ADDR = "127.0.0.1:1887"
+	URL        = "http://www.google.com"
+)
+
+func main() {
+	// create a socks5 dialer
+	dialer, err := proxy.SOCKS5("tcp", PROXY_ADDR, nil, proxy.Direct)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
+		os.Exit(1)
+	}
+	// setup a http client
+	httpTransport := &http.Transport{}
+	httpClient := &http.Client{Transport: httpTransport}
+	// set our socks5 as the dialer
+	httpTransport.Dial = dialer.Dial
+	// create a request
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't create request:", err)
+		os.Exit(2)
+	}
+	// use the http client to fetch the page
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't GET page:", err)
+		os.Exit(3)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading body:", err)
+		os.Exit(4)
+	}
+	fmt.Println(string(b))
+}
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/elazarl/goproxy"
+	"golang.org/x/net/proxy"
 )
 
 func doProxy(req *http.Request, ctx *goproxy.ProxyCtx, requestUtil *IRequestUtil, proxyHub *IProxyHub) (*http.Request, *http.Response) {
@@ -20,18 +24,72 @@ func doProxy(req *http.Request, ctx *goproxy.ProxyCtx, requestUtil *IRequestUtil
 	return req, res
 }
 
+func socksProxy(req *http.Request) *http.Response {
+	// create a socks5 dialer
+	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:1887", nil, proxy.Direct)
+	if err != nil {
+		// panic(err)
+		fmt.Println(err)
+		return nil
+	}
+	proxyGet := req.Header.Get("GET")
+	println(proxyGet)
+	// reader, err := req.GetBody()
+	// if err != nil {
+	// 	// panic(err)
+	// 	fmt.Println(err)
+	// 	return nil
+	// }
+	// fmt.Println(reader)
+	fmt.Println("======")
+	fmt.Println(req.Host)
+	fmt.Println(req.URL)
+	reqNew, err := http.NewRequest(req.Method, req.URL.String(), nil)
+	reqNew.Header = req.Header
+	reqNew.Body = req.Body
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't create request:", err)
+		os.Exit(2)
+	}
+	req = reqNew
+
+	fmt.Println("======")
+	fmt.Println(req.Host)
+	fmt.Println(req.URL)
+	// setup a http client
+	httpTransport := &http.Transport{}
+	httpClient := &http.Client{Transport: httpTransport}
+	// set our socks5 as the dialer
+	httpTransport.Dial = dialer.Dial
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		// panic(err)
+		fmt.Println(err)
+		return nil
+	}
+	println("return valid resp")
+	return resp
+}
+
 func main() {
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
-	addr := flag.String("addr", ":8080", "proxy listen address")
+	addr := flag.String("addr", ":8084", "proxy listen address")
 	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		if req.URL.Scheme == "https" {
-			// req.URL.Scheme = "http"
-			println(req.URL.Scheme)
+		if strings.Contains(req.URL.Host, "google") ||
+			strings.Contains(req.URL.Host, "youtube") ||
+			strings.Contains(req.URL.Host, "yt") ||
+			strings.Contains(req.URL.Host, "gsta") {
 			println(req.URL.Host)
+			resp := socksProxy(req)
+			return req, resp
 		}
+		// if req.URL.Scheme == "https" {
+		// 	// req.URL.Scheme = "http"
+		// 	println(req.URL.Scheme)
+		// }
 		return req, nil
 	})
 	proxy.Verbose = *verbose
