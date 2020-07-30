eb03420fc561c8b0cc8cda67f203787b82c7fcd3
kkito <kkitokkito@126.com>
2020-07-30 09:48:03 +0800

    覆盖的实现

--- util_test.go
+++ util_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIsCNHost(t *testing.T) {
 	isCNHost("www.baidu.com")
@@ -17,4 +20,71 @@ func TestGetIPFromHost(t *testing.T) {
 	if isCNHost("kkito.cn") {
 		t.FailNow()
 	}
+}
+
+/// ========= test embed struct
+
+type embStr struct {
+	name string
+	age  int
+}
+
+func (em *embStr) printName() string {
+	return em.name + " " + em.getAge()
+}
+
+func (em *embStr) getAge() string {
+	return "empty age"
+}
+
+type student struct {
+	embStr
+	no string
+}
+
+func (st *student) getAge() string {
+	return "age from student"
+}
+
+func TestStduent(t *testing.T) {
+	stu := student{}
+	stu.name = "test name"
+	// fmt.Println(stu.name)
+	ret := stu.printName()
+	fmt.Println(ret)
+	fmt.Println(stu.getAge())
+}
+
+// ===== override
+
+type I interface {
+	Foo() string
+}
+
+type A struct {
+	i I
+}
+
+func (a *A) Foo() string {
+	return "A.Foo()"
+}
+
+func (a *A) Bar() string {
+	return a.i.Foo()
+}
+
+type B struct {
+	A
+}
+
+func (b *B) Foo() string {
+	return "B.Foo()"
+}
+
+func TestOverride(t *testing.T) {
+	// 借助一个接口把父子关系串联起来
+	inst := B{}
+	inst.i = &inst
+	t.Log(inst.i)
+	t.Log(inst.Bar())
 }
